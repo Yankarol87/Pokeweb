b428eebc81cfa4c319b0036f4017d06638e6b6ad
wendy <wendymayoral55@hotmail.com>
2024-07-21 23:47:54 -0600

    Pokeweb

--- usecase/debilidad/DebilidadController.php
+++ usecase/debilidad/DebilidadController.php
@@ -1,18 +1,20 @@
 <?php
+require_once __DIR__ . '/../../Dto/DebilidadIn.php';
 require_once __DIR__ . '/DebilidadUseCase.php';
 require_once __DIR__ . '/DebilidadGateway.php';
-require_once __DIR__ . '/../../Dto/DebilidadIn.php';
+
 class DebilidadController {
-    public function InsertarDebilidad(DebilidadIn $debilidadObj){
+    public function InsertarDebilidad(DebilidadIn $debilidadObj) {
         $debilidadGateway = new DebilidadGateway();
-        $useCase = new DebilidadUseCase ($debilidadGateway);
-        $result = $useCase->Insertar ($debilidadObj);
+        $useCase = new DebilidadUseCase($debilidadGateway);
+        $result = $useCase->Insertar($debilidadObj);
         return $result;
     }
-public function ListarDebilidad(){
-    $debilidadGateway = new DebilidadGateway();
-    $useCase = new DebilidadUseCase ($debilidadGateway);
-    $result = $useCase->Listar();
-    return $result;
-}
+
+    public function ListarDebilidad() {
+        $debilidadGateway = new DebilidadGateway();
+        $useCase = new DebilidadUseCase($debilidadGateway);
+        $result = $useCase->Listar();
+        return $result;
+    }
 }
